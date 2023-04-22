206ed7eb5ee9aa79c90186b90cf3df2f4537261a
madtisa <temp-email-for-oauth-madtisa@gitlab.localhost>
2023-04-22 21:48:16 +0300

    Add support for base64 encoding.

--- src/async-lz-string.ts
+++ src/async-lz-string.ts
@@ -4,6 +4,59 @@ import { IDecompressor, DecompressorImpl } from "./decompressor";
 const compressor: ICompressor = new CompressorImpl();
 const decompressor: IDecompressor = new DecompressorImpl();
 
+const keyStrBase64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+const baseReverseDic: Record<string, Record<string, number>> = {};
+
+function getBaseValue(alphabet, character): number {
+  if (!baseReverseDic[alphabet]) {
+    baseReverseDic[alphabet] = {};
+    for (let i = 0; i < alphabet.length; i++) {
+      baseReverseDic[alphabet][alphabet.charAt(i)] = i;
+    }
+  }
+  return baseReverseDic[alphabet][character];
+}
+
+export async function compressToBase64(input: string) {
+  if (input === null) {
+    return "";
+  }
+  const compressResult = await compressor.compress(
+    input,
+    6,
+    function (a) {
+      return keyStrBase64.charAt(a);
+    }
+  );
+  switch (compressResult.length % 4) { // To produce valid Base64
+    default: // When could this happen ?
+    case 0 :
+      return compressResult;
+    case 1 :
+      return compressResult + "===";
+    case 2 :
+      return compressResult + "==";
+    case 3 :
+      return compressResult + "=";
+  }
+}
+
+export async function decompressFromBase64(compressed: string) {
+  if (compressed === null) {
+    return "";
+  }
+  if (compressed === "") {
+    return null;
+  }
+  return await decompressor.decompress(
+    compressed.length,
+    32,
+    function (index) {
+      return getBaseValue(keyStrBase64, compressed.charAt(index));
+    }
+  );
+}
+
 export async function compressToUTF16(input: string) {
   if (input === null) {
     return "";
--- test/async-lz-string-test.ts
+++ test/async-lz-string-test.ts
@@ -20,7 +20,6 @@ function testUtf16Compression(str: string): (done: () => void) => void {
                 expect(asyncLzResult).toEqual(originalLZResult);
                 done();
             });
-
     }
 }
 
@@ -28,6 +27,28 @@ function testUtf16Decompression(str: string): (done: () => void) => void {
     return (done) => {
         const originalLZCompressed = LZString.compressToUTF16(str);
         AsyncLZString.decompressFromUTF16(originalLZCompressed)
+            .then((decompressed) => {
+                expect(decompressed).toEqual(str);
+                done();
+            });
+    }
+}
+
+function testBase64Compression(str: string): (done: () => void) => void {
+    return (done) => {
+        AsyncLZString.compressToBase64(str)
+            .then((asyncLzResult) => {
+                const originalLZResult = LZString.compressToBase64(str);
+                expect(asyncLzResult).toEqual(originalLZResult);
+                done();
+            });
+    }
+}
+
+function testBase64Decompression(str: string): (done: () => void) => void {
+    return (done) => {
+        const originalLZCompressed = LZString.compressToBase64(str);
+        AsyncLZString.decompressFromBase64(originalLZCompressed)
             .then((decompressed) => {
                 expect(decompressed).toEqual(str);
                 done();
@@ -71,5 +92,42 @@ describe('async-lz-string', () => {
         it('correctly decompresses a random string compressed by lz-string', testUtf16Decompression(makeString(50000)));
 
         it('correctly decompresses a random long string compressed by lz-string', testUtf16Decompression(makeString(500000)));
+    });
+
+    describe('compressToBase64', () => {
+        it('produces the same output as original lz-string for a single-letter string', testBase64Compression("a"));
+
+        it('produces the same output as original lz-string for a string containing a single non-ASCII letter', testBase64Compression("å"));
+
+        it('produces the same output as original lz-string for a string containing non-ASCII characters', testBase64Compression("åäö空手おじいさんåäö"));
+
+        it('produces the same output as original lz-string for a mixed ASCII/non-ASCII string', testBase64Compression("abcåäö"));
+
+        it('produces the same output as original lz-string for a random string', testBase64Compression(makeString(50000)));
+
+        it('produces the same output as original lz-string for a random long string', testBase64Compression(makeString(500000)));
+
+        it('can be correctly decompressed', (done) => {
+            const str = "aaabbccccd";
+            AsyncLZString.compressToBase64(str)
+                .then((asyncLzResult) => {
+                    expect(LZString.decompressFromBase64(asyncLzResult)).toEqual(str);
+                    done();
+                });
+        });
+    });
+
+    describe('decompressFromBase64', () => {
+        it('correctly decompresses a single-letter string compressed by lz-string', testBase64Decompression("a"));
+
+        it('correctly decompresses a string containing a single non-ASCII letter compressed by lz-string', testBase64Decompression("å"));
+
+        it('correctly decompresses a string containing non-ASCII characters compressed by lz-string', testBase64Decompression("åäö空手おじいさんåäö"));
+
+        it('correctly decompresses a mixed ASCII/non-ASCII string compressed by lz-string', testBase64Decompression("abcåäö"));
+
+        it('correctly decompresses a random string compressed by lz-string', testBase64Decompression(makeString(50000)));
+
+        it('correctly decompresses a random long string compressed by lz-string', testBase64Decompression(makeString(500000)));
     });
 });
