2f5f5cd25c14d16e0fb60b44d7b0fca910621a13
Jack Bister <jack@jackbister.com>
2023-04-29 17:16:57 +0200

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
+const { join } = require("path");
+
 module.exports = {
+  mode: "development",
   devtool: "source-map",
   entry: {
-    "async-lz-string": __dirname + "/src/async-lz-string.ts",
+    "async-lz-string": join(__dirname, "src", "async-lz-string.ts"),
   },
   module: {
     rules: [{ test: /\.ts$/, loader: "ts-loader" }],
   },
   output: {
     libraryTarget: "umd",
-    path: __dirname + "/libs",
+    path: join(__dirname, "libs"),
     filename: "[name].js",
     globalObject: "this",
   },
